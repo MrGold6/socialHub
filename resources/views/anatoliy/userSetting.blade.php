5087c4dcd12f279e81d7fc1764f44a69636e135d
toleeek <denamusuk2003@gmail.com>
2021-11-27 17:15:35 +0200

    setting user

--- resources/views/anatoliy/userSetting.blade.php
+++ resources/views/anatoliy/userSetting.blade.php
@@ -1,27 +1,47 @@
 @extends('layout.template')
 @section('content')
-    <link rel = "stylesheet" type = "text/css" href="{{asset('anatoliy/styleSetting.css?3')}}" />
+    <link rel = "stylesheet" type = "text/css" href="{{asset('anatoliy/styleSetting.css?4')}}" />
 
     <div class="container">
 
         @foreach($settings as $setting)
             <div class="row">
-                <div class="col-xl-5 col-lg-5 col-md-12 col-sm-12 col-xs-12 upload-btn-wrapper text-center">
+                <div class="col-xl-6 col-lg-6 col-md-12 col-sm-12 col-xs-12 upload-btn-wrapper text-center">
                     <img class="" style="width: 300px; border-radius: 15px;" src="data:image/jpeg;base64,{{ base64_encode($setting->image) }}"/><br />
-                       <div class="cont">
-                           <input type="file" class="form-text" />
-                       </div>
+                    <input type="file" class="form-text" />
                     <button class="btnDang px-3">Видалити фото</button>
                 </div>
-                <div class="col-xl-7 col-lg-7 col-md-12 col-sm-12 col-xs-12 text-center">
-                    <label style="color: #526bb9; font-size: 25px" class="mt-2">Ім'я: </label>
-                    <br /><input class="inputname datainfo" type="text" value="{{$setting->firstName}}">
-                    <br />
-                    <label style="color: #526bb9; font-size: 25px" class="mt-2">Прізвище: </label>
-                    <br /> <input class="inputname datainfo" type="text" value="{{$setting->middleName}}">
-                    <br />
-                    <label style="color: #526bb9; font-size: 25px" class="mt-2">По-батькові: </label><br />
-                    <input class="inputname datainfo" type="text" value="{{$setting->lastName}}">
+                <div class="col-xl-6 col-lg-6 col-md-12 col-sm-12 col-xs-12">
+                    <div class="text-cnter">
+                        <p><label style="color: #526bb9; font-size: 25px" class="">Ім'я: </label></p>
+
+                        <input class="inputname datainfo " type="text" value="{{$setting->firstName}}">
+                    </div>
+
+                    <div class="text-cnter">
+                        <p><label style="color: #526bb9; font-size: 25px" class="mt-2">Прізвище: </label></p>
+
+                        <input class="inputname datainfo" type="text" value="{{$setting->middleName}}">
+                    </div>
+
+                    <div class="text-cnter">
+                        <p><label style="color: #526bb9; font-size: 25px" class="mt-2">По-батькові: </label></p>
+                        <input class="inputname datainfo" type="text" value="{{$setting->lastName}}">
+                    </div>
+
+                    <div class="text-cnter">
+                        <p><label style="color: #526bb9; font-size: 25px" class="mt-2">E-mail: </label></p>
+                        <input class="inputname datainfo" type="email" value="{{$setting->email}}">
+                    </div>
+
+                    <div class="text-cnter">
+                        <p><label style="color: #526bb9; font-size: 25px" class="mt-2">День народження: </label></p>
+                        <input class="inputname datainfo" type="date" value="{{$setting->birthday}}">
+                    </div>
+
+                    <div class="text-cnter">
+                        <a class="btn btn-siniy" href="#" role="button">Змінити данні</a>
+                    </div>
                 </div>
 
             </div>
